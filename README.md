fe760a66cea1b486dc3bad17de9355d715941932
lovelyhwado <lovelyhwado@gmail.com>
2016-06-15 13:43:29 +0900

    create a project

---
+++ README.md
@@ -0,0 +1,2 @@
+# CourseRegistration
+- 귀찮아서 gitignore같은건 안만듬 
---
+++ src/main/java/com/ddd/CourseRegistrationApplication.java
@@ -0,0 +1,12 @@
+package com.ddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseRegistrationApplication.class, args);
+	}
+}
