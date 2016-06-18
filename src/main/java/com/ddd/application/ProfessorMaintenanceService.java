34bb17a22d34b98d64fc3d47a75d3aa3c6ac1874
koliaok <Lak@choigwanggyuui-MacBook-Pro.local>
2016-06-18 22:02:17 +0900

    add com.ddd.Application and com.ddd.application.impl Professor

--- src/main/java/com/ddd/application/ProfessorMaintenanceService.java
+++ src/main/java/com/ddd/application/ProfessorMaintenanceService.java
@@ -1,5 +1,11 @@
 package com.ddd.application;
 
+import com.ddd.domain.model.professor.ProfessorCourse;
+import com.ddd.domain.model.professor.ProfessorID;
+
 public interface ProfessorMaintenanceService {
 
+	ProfessorID addNewProfessorID(String name);
+	ProfessorCourse addNewProfessorCourseCourse(String name, String time);
+	
 }
--- src/main/java/com/ddd/application/impl/ProfessorMaintenanceServiceImpl.java
+++ src/main/java/com/ddd/application/impl/ProfessorMaintenanceServiceImpl.java
@@ -1,5 +1,30 @@
 package com.ddd.application.impl;
 
-public class ProfessorMaintenanceServiceImpl {
+import com.ddd.application.ProfessorMaintenanceService;
+import com.ddd.domain.model.course.CourseOfferingRepository;
+import com.ddd.domain.model.professor.ProfessorCourse;
+import com.ddd.domain.model.professor.ProfessorID;
+import com.ddd.domain.model.professor.ProfessorRepository;
+
+public class ProfessorMaintenanceServiceImpl implements ProfessorMaintenanceService{
+
+	private final ProfessorRepository professorRepository;
+	private final CourseOfferingRepository courseOfferingRepository;
+	
+	public ProfessorMaintenanceServiceImpl(final ProfessorRepository professorRepository,
+			final CourseOfferingRepository courseOfferingRepository) {
+		this.professorRepository = professorRepository;
+		this.courseOfferingRepository = courseOfferingRepository;
+	}
+	@Override
+	public ProfessorID addNewProfessorID(String name)
+	{
+		return null;
+	}
+	
+	@Override
+	public ProfessorCourse addNewProfessorCourseCourse(String name, String time) {
+		return null;
+	}
 
 }
