1e2abec717cd598ea1a9e26c986864ea949e207e
koliaok <Lak@choigwanggyuui-MacBook-Pro.local>
2016-06-18 11:06:42 +0900

    add professor service application Implementation

--- src/main/java/com/ddd/domain/model/service/ProfessorMaintenanceService.java
+++ src/main/java/com/ddd/domain/model/service/ProfessorMaintenanceService.java
@@ -1,5 +1,10 @@
 package com.ddd.domain.model.service;
 
+import com.ddd.domain.model.professor.ProfessorID;
+
 public interface ProfessorMaintenanceService {
 
+	ProfessorID add();
+	ProfessorID delete();
+	ProfessorID update();
 }
