25525444cb3138fa10f93aec19f0f20f2c844201
koliaok <Lak@choigwanggyuui-MacBook-Pro.local>
2016-06-19 23:18:52 +0900

    add domain Service Professor

--- src/main/java/com/ddd/domain/service/ProfessorMaintenanceService.java
+++ src/main/java/com/ddd/domain/service/ProfessorMaintenanceService.java
@@ -4,7 +4,8 @@ import com.ddd.domain.model.professor.Professor;
 import com.ddd.domain.model.professor.ProfessorID;
 
 public interface ProfessorMaintenanceService {
-	void CourseAdd();
-	void Coursedelete();
+	void CourseAdd(Professor professer, String course);
+	void Coursedelete(Professor professer, String course);
+	
 	
 }
--- src/main/java/com/ddd/domain/service/impl/ProfessorMaintenanceServiceImpl.java
+++ src/main/java/com/ddd/domain/service/impl/ProfessorMaintenanceServiceImpl.java
@@ -1,15 +1,18 @@
 package com.ddd.domain.service.impl;
 
+import com.ddd.domain.model.professor.Professor;
 import com.ddd.domain.service.ProfessorMaintenanceService;
 
 public class ProfessorMaintenanceServiceImpl implements ProfessorMaintenanceService{
 	@Override
-	public void CourseAdd()
+	public void CourseAdd(Professor professor, String course)
 	{	
+		professor.getCourse().add(course);
 	}
 	@Override
-	public void Coursedelete()
+	public void Coursedelete(Professor professor, String course)
 	{
+		professor.getCourse().remove(course);
 	}
 	
 
